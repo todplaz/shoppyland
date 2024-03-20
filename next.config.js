986df428f1acdeef0fd101fb0630adef5a699306
todplaz <balocm@yahoo.fr>
2024-03-20 23:00:17 +0100

    first commit

---
+++ next.config.js
@@ -0,0 +1,25 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    // domains: ['lh3.googleusercontent.com','files.stripe.com']
+    //I changed the original domains: with remotePaterns: because of deprecation
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'files.stripe.com',
+      },
+    ],
+  },
+}
+
+module.exports = nextConfig
